8ebce06fb27e5094df31ccbc4eb96ddeb918df35
Galdino0800 <galdinojua@gmail.com>
2026-03-06 21:23:40 -0300

    update: backup automático

--- polybar/scripts/git_status.sh
+++ polybar/scripts/git_status.sh
@@ -2,14 +2,14 @@
 REPO="/home/ghost/dotfiles"
 
 if [ ! -d "$REPO/.git" ]; then
-    echo "󰊤 Path Errado"
+    echo "Erro Path"
     exit
 fi
 
 STATUS=$(git -C "$REPO" status -uno 2>/dev/null | grep -c "ahead")
 
 if [ "$STATUS" -gt 0 ]; then
-    echo "󰊤 Out of Sync"
+    echo "Pendente"
 else
-    echo "󰊤 Synced"
+    echo "Sincronizado"
 fi
--- polybar/scripts/scroll_tidal_status.sh
+++ polybar/scripts/scroll_tidal_status.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-
-# O nome exato que o seu sistema mostrou no playerctl -l
 PLAYER="io.github.nokse22.high-tide"
-
-# Tenta pegar artista e título
 metadata=$(playerctl -p $PLAYER metadata --format "{{ artist }} - {{ title }}" 2>/dev/null)
 
 if [ -z "$metadata" ]; then
-    echo "" # Fica invisível se não tiver música
+    echo "" 
 else
-    # Mostra o ícone e a música (limitado a 25 caracteres)
-    echo "󰓇 ${metadata:0:25}"
+    # REMOVA o ícone daqui, deixe só o texto
+    echo "${metadata:0:25}"
 fi
