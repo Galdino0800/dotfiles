10661ca7505e2db725cd042c9f37b2fede708068
Galdino0800 <galdinojua@gmail.com>
2026-03-25 21:17:14 -0300

    update: backup automático

--- i3/volume_notify.sh
+++ i3/volume_notify.sh
@@ -1,11 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Pega o volume atual e o status (Mudo ou não)
-VOLUME=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '[0-9]+(?=%)' | head -1)
+# Pega o volume atual e o status de mute (usando o pactl que é padrão no Arch)
+VOLUME=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '[0-9]+(?=%)' | head -n 1)
 MUTE=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')
 
-if [ "$MUTE" = "yes" ]; then
-    notify-send -u low -t 1000 "Volume" "Mudo [off]"
+# Define o ícone com base no volume e no mute
+if [ "$MUTE" = "yes" ] || [ "$VOLUME" -eq 0 ]; then
+    ICON="󰝟"
+    TEXT="Mudo"
+elif [ "$VOLUME" -lt 30 ]; then
+    ICON="󰕿"
+    TEXT="Baixo"
+elif [ "$VOLUME" -lt 70 ]; then
+    ICON="󰖀"
+    TEXT="Médio"
 else
-    notify-send -u low -t 1000 "Volume" "Nível: $VOLUME%"
+    ICON="󰕾"
+    TEXT="Alto"
 fi
+
+# A MÁGICA: O Dunst aceita uma "stack tag" para não empilhar várias notificações
+# e o valor "value" cria a barra de progresso automática
+dunstify -a "Volume" -u low -h string:x-dunst-stack-tag:volume -h int:value:"$VOLUME" "$ICON  $VOLUME%" "$TEXT"
